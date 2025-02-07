0967e982bd60ef6198d03ab7efac7d55d15477f0
sytnic <sytnic@list.ru>
2025-02-07 14:52:32 +0300

    15. My comments

--- functions/echo_input.php
+++ functions/echo_input.php
@@ -5,8 +5,10 @@ function echo_input($update) {
   $update->method[0] = 'sendMessage';
 
   // It's going back to the same chat where it came from
+  // Оно возвращается в тот же чат, откуда пришло
   $update->post_fields[0]->chat_id = $update->message->chat->id;
 
   // The text being returned is just the whole object
+  // Возвращаемый текст - это просто весь объект целиком
   $update->post_fields[0]->text = print_r($update, TRUE);
 }
--- index.php
+++ index.php
@@ -7,13 +7,17 @@ include_once 'functions/echo_input.php';
 include_once 'functions/send_response.php';
 
 // Grab the JSON input stream from Telegram, convert it to an object
+// Возьмите входной поток JSON из Telegram, преобразуйте его в объект
 $update = json_decode(file_get_contents('php://input'));
 
 // Initialize two variables used to respond to Telegram.
 // (Arrays allow for multiple responses to be sent to Telegram.)
+// Инициализируем две переменные, используемые для отправки ответов в Telegram.
+// (Массивы позволяют отправлять в Telegram несколько ответов.)
 $update->method = array();
 $update->post_fields = array();
 // There will always be at least one response
+// Всегда будет по крайней мере один ответ
 $update->post_fields[0] = new \stdClass();
 
 // Do the thing
